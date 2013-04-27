962969488f0b0d03b2dcb74f59bcd75a3479ee4c
Brian <nairb85@gmail.com>
2013-04-27 15:26:23 -0700

    Initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArkhamHelper::Application.config.secret_token = '676bf78e49843baa2fe932f0b956413fa42544d45f41fa34c731cfac23c527b1fab65b84026c4621ff849a25a626c4b9664e50e26d12960b52e3edd190c6ce39'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ArkhamHelper::Application.config.session_store :cookie_store, :key => '_ArkhamHelper_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ArkhamHelper::Application.config.session_store :active_record_store
